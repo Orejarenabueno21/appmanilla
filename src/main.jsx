10152f1d9b2a037627c8a8315cb90046a86aab02
raul <orejarena1999@gmail.com>
2023-05-29 22:46:48 -0500

    c

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-<<<<<<< HEAD
-=======
-import Formulario from './assets/components/Formulario'
->>>>>>> develop
 import './index.css'
 import Formulario from './assets/components/Formulario'
 
