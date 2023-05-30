9065879666b3d06b0580226ad9a9fec893dc2e54
raul <orejarena1999@gmail.com>
2023-05-29 22:54:21 -0500

    commit de prueba

--- src/assets/components/Formulario.jsx
+++ src/assets/components/Formulario.jsx
@@ -1,15 +1,11 @@
-<<<<<<< HEAD
-import React from 'react'
-=======
-import React,{useState, useEffect} from 'react'
+import {React,useState, useEffect} from 'react'
 import { db } from '../firebase'
 import {collection, doc, addDoc, onSnapshot, deleteDoc, updateDoc} from 'firebase/firestore'
->>>>>>> develop
 
-const Formulario = () => {
-  return (
-    <div>Formulario</div>
-  )
+function Formulario() {
+    return (
+        <div>Formulario</div>
+    )
 }
 
 export default Formulario
